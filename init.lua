f118c83b2c2b14bed50120b3f46a762448a842ab
Eric <ericff@gmail.com>
2024-04-19 19:17:30 +0800

    fix: ishortPage

--- init.lua
+++ init.lua
@@ -1,24 +1,22 @@
-
-    -- image[0,5.2;1,1;gui_hb_bg.png]
-		-- image[1,5.2;1,1;gui_hb_bg.png]
-		-- image[2,5.2;1,1;gui_hb_bg.png]
-		-- image[3,5.2;1,1;gui_hb_bg.png]
-		-- image[4,5.2;1,1;gui_hb_bg.png]
-		-- image[5,5.2;1,1;gui_hb_bg.png]
-		-- image[6,5.2;1,1;gui_hb_bg.png]
-		-- image[7,5.2;1,1;gui_hb_bg.png]
 local theme_inv = [[
     style_type[list;size=1.16,1.16;spacing=0,0] 
 		list[current_player;main;0,5.2;8,1;]
+		list[current_player;main;0,6.35;8,3;8]
 	]]
-		-- list[current_player;main;0,6.35;8,3;8]
+
+local short_theme_inv = [[
+    style_type[list;size=1.16,1.16;spacing=0,0] 
+		list[current_player;main;0,5.2;8,1;]
+	]]
 
 function sfinv.make_formspec(player, context, content, show_inv, size)
+  local pageName = sfinv.pages_unordered[context.nav_idx].name
+  local ishortPage = pageName == 'creative:all' or pageName == 'creative:nodes' or pageName == 'creative:tools' or pageName == 'creative:craftitems'
+
 	local tmp = {
-		-- size or "size[8,9.1]",
-		size or "size[8,6]",
+		size or (ishortPage and "size[8,6]" or "size[8,9.1]"),
 		sfinv.get_nav_fs(player, context, context.nav_titles, context.nav_idx),
-		show_inv and theme_inv or "",
+		show_inv and (ishortPage and short_theme_inv or theme_inv) or "",
 		content,
 	}
 	return table.concat(tmp, "")
